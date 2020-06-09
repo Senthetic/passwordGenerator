cd55306055a9c1bbd2098456c2173295e9c3f03e
Andrew Collier <49452922+Senthetic@users.noreply.github.com>
2020-06-08 23:07:25 -0400

    made it prettier

--- script.js
+++ script.js
@@ -14,100 +14,96 @@
 // THEN a password is generated that matches the selected criteria
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
- var generateBtn = document.querySelector("#generate");
- var alphabet = "abcdefghijklmnopqrstuvwxyz";
- var numbs ="0123456789";
- var specialChars = " !#$%&'()*+-,./:;<>=?@[]{}\^_`~";
- var passLength = prompt("How long would you like your password? (1-8)");
- console.log(passLength);
- var parsedLength = parseInt(passLength);
- console.log(parsedLength);
- var numLower = prompt("How many lowercase letters?");
- var parsedLower = parseInt(numLower);
- var numHigher = prompt("How many uppercase letters?");
- var parsedHigher = parseInt(numHigher);
- var numNumb = prompt("How many numbers?");
- var parsedNumb = parseInt(numNumb);
- var numSpecial = prompt("How many special characters?");
- var parsedSpecial = parseInt(numSpecial);
+var generateBtn = document.querySelector("#generate");
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var numbs = "0123456789";
+var specialChars = " !#$%&'()*+-,./:;<>=?@[]{}^_`~";
+var passLength = prompt("How long would you like your password? (1-8)");
+console.log(passLength);
+var parsedLength = parseInt(passLength);
+console.log(parsedLength);
+var numLower = prompt("How many lowercase letters?");
+var parsedLower = parseInt(numLower);
+var numHigher = prompt("How many uppercase letters?");
+var parsedHigher = parseInt(numHigher);
+var numNumb = prompt("How many numbers?");
+var parsedNumb = parseInt(numNumb);
+var numSpecial = prompt("How many special characters?");
+var parsedSpecial = parseInt(numSpecial);
 
- var currentPassword = "";
+var currentPassword = "";
 
 // Write password to the #password input
 function writePassword() {
-  
   console.log("writePassword Called");
   var password = generatePassword();
   console.log(password);
-  
+
   var passwordText = document.querySelector("#password");
   //event.preventDefault;
   passwordText.textContent = password;
   //document.body.appendChild(passwordText);
   alert("Your customized password is: " + password);
-
 }
-function generatePassword(){
-console.log("generatePassword entered");
+function generatePassword() {
+  console.log("generatePassword entered");
 
-  for(var i = 0; i<=parsedLength;i++){
+  for (var i = 0; i <= parsedLength; i++) {
     console.log("loop started");
-    var typePass = getRandomIntInclusive(1,4);
+    var typePass = getRandomIntInclusive(1, 4);
     console.log("typepass = " + typePass);
     //if there is no more available slots for the character type increase or decrease typePass
-    if((typePass == 1 &&parsedNumb==0)||(typePass == 2&&parsedHigher ==0)||(typePass==3&&parsedLower==0)||(typePass==4&&parsedSpecial==0)){
-      if(typePass==4&&parsedLower!=0){
+    if (
+      (typePass == 1 && parsedNumb == 0) ||
+      (typePass == 2 && parsedHigher == 0) ||
+      (typePass == 3 && parsedLower == 0) ||
+      (typePass == 4 && parsedSpecial == 0)
+    ) {
+      if (typePass == 4 && parsedLower != 0) {
         console.log("typePass decreased");
         typePass--;
-      }
-      else{
+      } else {
         console.log("typePass increased");
         typePass++;
       }
     }
-    if(typePass == 1&&parsedNumb!=0){
-      var x = getRandomIntInclusive(0,10);
+    if (typePass == 1 && parsedNumb != 0) {
+      var x = getRandomIntInclusive(0, 10);
       console.log(x);
-      currentPassword =currentPassword.concat(x);
+      currentPassword = currentPassword.concat(x);
       console.log(currentPassword);
       parsedNumb--;
-    }
-    else if(typePass == 2&&parsedHigher!=0){
-      var x = getRandomIntInclusive(0,26);
-      var y = (alphabet.charAt(x)).toUpperCase();
-      console.log(x,y);
+    } else if (typePass == 2 && parsedHigher != 0) {
+      var x = getRandomIntInclusive(0, 26);
+      var y = alphabet.charAt(x).toUpperCase();
+      console.log(x, y);
       currentPassword = currentPassword.concat(y);
       console.log(currentPassword);
       parsedHigher--;
-    }
-    else if(typePass == 3&&parsedLower!=0){
-      var x = getRandomIntInclusive(0,26);
+    } else if (typePass == 3 && parsedLower != 0) {
+      var x = getRandomIntInclusive(0, 26);
       var y = alphabet.charAt(x);
-      console.log(x,y);
+      console.log(x, y);
       currentPassword = currentPassword.concat(y);
       console.log(currentPassword);
       parsedLower--;
-    }
-    else if(typePass == 4&&parsedSpecial!=0){
-      var x = getRandomIntInclusive(0,26);
+    } else if (typePass == 4 && parsedSpecial != 0) {
+      var x = getRandomIntInclusive(0, 26);
       var y = specialChars.charAt(x);
-      console.log(x,y);
-      currentPassword =  currentPassword.concat(y);
+      console.log(x, y);
+      currentPassword = currentPassword.concat(y);
       console.log(currentPassword);
       parsedHigher--;
+    } else {
     }
-    else{
-
-    }
-    
-}
-console.log(currentPassword);
-return currentPassword;
+  }
+  console.log(currentPassword);
+  return currentPassword;
 }
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
 
 // Add event listener to generate button
